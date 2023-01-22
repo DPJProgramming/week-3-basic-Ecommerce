94972c5c358e9c1926dabae90c06ededd1672a4d
cheesy d <cheesyd64@hotmail.com>
2023-01-22 12:33:49 -0800

    Add project files.

---
+++ week 3 basic Ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace week_3_basic_Ecommerce.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

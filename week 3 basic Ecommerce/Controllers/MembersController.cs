0885b4af01e8713f9bcab43f291a8e16d30781e8
cheesy d <cheesyd64@hotmail.com>
2023-02-24 12:58:04 -0800

    Added Http Post method for register

--- week 3 basic Ecommerce/Controllers/MembersController.cs
+++ week 3 basic Ecommerce/Controllers/MembersController.cs
@@ -1,10 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using week_3_basic_Ecommerce.Data;
+using week_3_basic_Ecommerce.Models;
 
 namespace week_3_basic_Ecommerce.Controllers {
     public class MembersController : Controller {
 
+        private readonly VideoGameContext _context;//field for access database
+
+        public MembersController(VideoGameContext context) {//constructor to create object from context class
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Register() {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel regModel) {
+
+            //add to database
+            if (ModelState.IsValid) {
+                Member newMember = new() {
+                    Email = regModel.Email,
+                    Password = regModel.Password,
+                };
+                _context.Members.Add(newMember);
+                await _context.SaveChangesAsync();
+
+                //redirect to home page
+                return RedirectToAction("Index", "Home");
+            }
+            return View(regModel);
         }
     }
 }

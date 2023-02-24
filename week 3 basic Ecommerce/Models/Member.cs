b289388244ce23d1bc19d568070222cbbdabec08
cheesy d <cheesyd64@hotmail.com>
2023-02-24 11:14:46 -0800

    created registerview model

--- week 3 basic Ecommerce/Models/Member.cs
+++ week 3 basic Ecommerce/Models/Member.cs
@@ -17,4 +17,25 @@ namespace week_3_basic_Ecommerce.Models {
 
         public string UserName { get; set; }
     }
+
+    public class RegisterViewModel {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)] //(maximum, minimum length)
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
 }
